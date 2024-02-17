23ab206c211bf5eb244776d21b4f318f56053a42
EfeTurkmen1 <155842204+EfeTurkmen1@users.noreply.github.com>
2024-02-17 20:13:33 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,88 @@
+import discord
+from discord.ext import commands
+import os
+import random
+import requests
+
+print(os.listdir('images'))
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+    
+@bot.command()
+async def basketball(ctx):
+    resimler_listesi = os.listdir("images")
+    rastgele_resim = random.choice(resimler_listesi)
+    with open(f'images/{rastgele_resim}', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+
+@bot.command()
+async def animals(ctx):
+    resimler_listesi = os.listdir("animals")
+    rastgele_resim = random.choice(resimler_listesi)
+    with open(f'animals/{rastgele_resim}', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+
+@bot.command()
+async def football(ctx):
+    resimler_listesi = os.listdir("football")
+    rastgele_resim = random.choice(resimler_listesi)
+    with open(f'football/{rastgele_resim}', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+
+@bot.command()
+async def games(ctx):
+    resimler_listesi = os.listdir("games")
+    rastgele_resim = random.choice(resimler_listesi)
+    with open(f'games/{rastgele_resim}', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+
+@bot.command()
+async def mem(ctx):
+    resimler_listesi = os.listdir("mem")
+    rastgele_resim = random.choice(resimler_listesi)
+    with open(f'mem/{rastgele_resim}', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+
+bot.run("MTE5ODMzMjIwMzczODg1NzU1Mg.G_J5qZ.nJZTtpIktJ7W_3aT0Ou3A0ukehsMzil6MLlksI")
